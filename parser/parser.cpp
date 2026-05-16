d665ba5966f8b10d6154273794f39b4957d14c33
Nannigalaxy <nannigalaxy@gmail.com>
2026-05-16 17:04:33 +0530

    read timestamp and type convertion handling

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -1,11 +1,17 @@
 #include "parser.h"
 
-#include <chrono>
 #include <fstream>
 #include <iostream>
 
 extern "C" {
 #include "../external/c_library_v2/common/mavlink.h"
+}
+
+static uint64_t read_be_u64(const uint8_t *b) {
+  return ((uint64_t)b[0] << 56) | ((uint64_t)b[1] << 48) |
+         ((uint64_t)b[2] << 40) | ((uint64_t)b[3] << 32) |
+         ((uint64_t)b[4] << 24) | ((uint64_t)b[5] << 16) |
+         ((uint64_t)b[6] << 8) | ((uint64_t)b[7]);
 }
 
 void Parser::parse_file(const std::string &filename) {
@@ -17,34 +23,45 @@ void Parser::parse_file(const std::string &filename) {
     return;
   }
 
-  mavlink_message_t msg;
   mavlink_status_t status;
+  mavlink_message_t msg;
 
-  uint8_t byte;
+  while (true) {
 
-  uint64_t timestamp_us = 0;
+    uint8_t tsbuf[8];
 
-  while (file.read(reinterpret_cast<char *>(&byte), 1)) {
-    if (mavlink_parse_char(MAVLINK_COMM_0, byte, &msg, &status)) {
-      TelemetryMessage tm;
+    file.read(reinterpret_cast<char *>(tsbuf), 8);
 
-      // synthetic timing
-      tm.timestamp_us = timestamp_us;
-
-      tm.message = msg;
-
-      messages.push_back(tm);
-
-      // increment fake clock
-      timestamp_us += 10000;
+    if (!file.good()) {
+      break;
     }
 
-    parse_errors = status.packet_rx_drop_count;
+    uint64_t timestamp_us = read_be_u64(tsbuf);
+    bool packet_complete = false;
+
+    while (!packet_complete && file.good()) {
+      uint8_t byte;
+      file.read(reinterpret_cast<char *>(&byte), 1);
+
+      if (!file.good()) {
+        break;
+      }
+
+      if (mavlink_parse_char(MAVLINK_COMM_0, byte, &msg, &status)) {
+        TelemetryMessage tm;
+
+        tm.timestamp_us = timestamp_us;
+        tm.message = msg;
+        messages.push_back(tm);
+        packet_complete = true;
+      }
+
+      parse_errors = status.packet_rx_drop_count;
+    }
   }
 
   file.close();
 
   std::cout << "Parsed messages: " << messages.size() << std::endl;
-
   std::cout << "Parse errors: " << parse_errors << std::endl;
 }
